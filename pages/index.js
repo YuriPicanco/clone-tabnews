cfb9381a90b8b6a3b163a06994a8af252687bda6
Yuri D. Souza <yuri.s.picanco@gmail.com>
2025-06-20 16:56:49 -0300

    Editando a página apenas para um desafio rápido. Adicionando uma imagem e formatação do html gerado pelo next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Image from 'next/image'
+import foto from '../assets/por-do-sol.jpg';
+
+const bordas = {
+  padding: 0,
+  margin: 0
+}
+
 function Home(){
-  return <h1>Teste</h1>
+  return <div style={{...bordas}}>
+            <div style={{backgroundColor: 'black', height: '95vh', alignItems: 'center', ...bordas}}>
+              <div style={{backgroundColor: 'white', textAlign: 'center', ...bordas, fontSize: '40px'}}>
+                Vamos assistir ao pôr-do-sol?
+              </div>
+              <Image 
+                /*fill*/
+                src={foto}
+                style={{objectFit: 'cover', height: '95vh', width: '100vw'}}
+              />
+
+            </div>
+        </div>
 }
 
 export default Home;
