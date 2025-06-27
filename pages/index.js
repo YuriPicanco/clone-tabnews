57b7ea8f24a33e8f37dc8d3a45340957abdd0570
Yuri D. Souza <yuri.s.picanco@gmail.com>
2025-06-27 07:14:16 -0300

    reorganizando as pastas

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,5 @@
-'use  client';
-import Player from './Player';
-import Image from 'next/image'
-import foto from '../public/por-do-sol.jpg';
-
-
-
 function Home(){
-  const bordas = {
-    padding: 0,
-    margin: 0
-  }
-  
-
-
-
-  return <div style={{...bordas}}>
-            <div style={{backgroundColor: 'white', height: '10vh', alignItems: 'center', ...bordas, display: 'flex', justifyContent: 'space-between'}}>              
-               <div style={{backgroundColor: 'white', textAlign: 'center', ...bordas, fontSize: '40px', paddingLeft: '0px'}}>
-                  Vamos assistir ao pôr-do-sol?
-              </div>
-
-              <div>
-                  <Player/>
-              </div>
-            </div>
-              <Image 
-                /*fill*/
-                src={foto}
-                style={{objectFit: 'cover', height: '90vh', width: '100vw'}}
-              />
-
-            </div>
+  return  <div>Teste</div>
 }
 
-export default Home;
+export default Home
