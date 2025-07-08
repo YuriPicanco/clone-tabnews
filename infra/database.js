6789c75a9358c5be27aac4a3f0d201bcd5b8c87e
Yuri D. Souza <yuri.s.picanco@gmail.com>
2025-07-08 11:04:16 -0300

    criando e conectando módulo

---
+++ infra/database.js
@@ -0,0 +1,27 @@
+import { Client } from "pg";
+
+const login = {
+   host: process.env.POSTGRES_HOST,
+   port: process.env.POSTGRES_PORT,
+   user: process.env.POSTGRES_USER,
+   database: process.env.POSTGRES_DB,
+   password: process.env.POSTGRES_PASSWORD,
+};
+
+async function query(queryObject) {
+   const client = new Client(login);
+   await client.connect();
+
+   const result = await client.query(
+      queryObject,
+      //"SELECT ROUND($1::numeric, 2) as Message",[4.0],
+   );
+
+   await client.end();
+
+   return result;
+}
+
+export default {
+   query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+   //console.log("database: " + database.query);
+   const result = await database.query("SELECT 1+1 as result");
+   console.log(result.rows);
+
    response.status(200).json({ status: "Online" });
 }
 
